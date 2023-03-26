82b5af1f62b5a37bdca393f50303e5c11e7b9ed5
Vladislav Kakorin <vlad.kakorin03@gmail.com>
2023-03-26 03:20:26 +0300

    feat: mobile version almost done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 let offset = 0; 
+const sliders = document.querySelectorAll('.slide');
 const sLine = document.querySelector('.slider-line');
 const prev = document.querySelector('.b__prev');
 const next = document.querySelector('.b__next');
 
+const sStyles = window.getComputedStyle(document.querySelector('.slide')); 
+const w = document.querySelector('.slide').offsetWidth;
+const marg = Number(sStyles.marginRight.slice(0, -2));
+function init() {
+  const w = document.querySelector('.slide').offsetWidth;
+  console.log(w)
+  console.log(w*sliders.length)
+  console.log(marg)
+  // sLine.style.width = w * sliders.length + 160 + 'px';
+  // sliders.forEach(e => {
+  //   e.style.width = w + 'px';
+  // });
+}
+
+init();
+
 next.addEventListener('click', function () {
-  offset -= 340;
-  if (offset < -1020) {
+  
+  offset -= w + (marg * 2);
+  if (offset < -w * sliders.length){
     offset = 0;
   }
   sLine.style.left = offset + 'px';
 })
 
 prev.addEventListener('click', function () {
-  offset += 340;
-  if (offset >= 1020) {
-    offset = 0;
+  offset += w + (marg * 2);
+  if (offset > 0) {
+    offset = -w * sliders.length + (sliders.length * marg * 2);
   }
   sLine.style.left = offset + 'px';
 })
+
+
+
